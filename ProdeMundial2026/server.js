0676022a77e6508bf833da4f3384f426b14e0363
dusan0415 <dusan0415@users.noreply.github.com>
2026-03-15 18:54:31 -0300

    configuracion para render

--- ProdeMundial2026/server.js
+++ ProdeMundial2026/server.js
@@ -335,18 +335,9 @@ VALUES(?,?,?)`,
 
 })
 
+const PORT = process.env.PORT || 3000
 
-
-
-
-
-
-
-
-
-
-
-app.listen(3000, () => {
-    console.log("Servidor corriendo en http://localhost:3000");
-});
+app.listen(PORT, () => {
+    console.log("Servidor corriendo en puerto " + PORT)
+})
 
